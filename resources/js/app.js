e467879bb69b94f84e7aef6cb5ad98129e1eb57b
Bass El Hachem <basselhachem@kompo.io>
2020-09-16 11:07:02 -0400

    Requiring optional components

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,5 +5,10 @@ window.Vue = require('vue');
 //Requiring vue-kompo after Vue has been set
 require('vue-kompo');
 
-//booting Vue
+//Requiring Kompo optional components
+require('kompo-ckeditor')
+require('kompo-googlemaps')
+require('kompo-trix')
+
+//Booting the app!
 const app = new Vue({ el: '#app' });
